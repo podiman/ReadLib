0738169dfd686759df7bc96adfe98096e57595d6
Mayura Wijeyaratne <mayura.wijeyaratne@adp.com>
2020-11-19 21:28:08 -0500

    Initial setup of React Typescript Sass project for ReadLib

---
+++ src/components/TextField.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+interface Props {
+    text: string;
+}
+
+const TextField: React.FC<Props> = ({text}) => {
+    return (
+        <div>
+            {text}
+        </div>
+    )
+}
+
+export default TextField;
